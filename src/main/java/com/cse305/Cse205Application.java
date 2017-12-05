8744b67c8a7167f6c2b5ab1b477b45cde564b840
Shawn <felixshawn1@gmail.com>
2017-12-05 18:17:45 -0500

    project setup

---
+++ src/main/java/com/cse305/Cse205Application.java
@@ -0,0 +1,12 @@
+package com.cse305;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cse205Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cse205Application.class, args);
+	}
+}
