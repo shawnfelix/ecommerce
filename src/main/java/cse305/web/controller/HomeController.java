c45317050d056eb967506fdfbe8f224cf3ce955b
Shawn <felixshawn1@gmail.com>
2017-12-05 21:43:06 -0500

    more project setup - added spring devtools and thymeleaf

--- src/main/java/cse305/web/controller/HomeController.java
+++ src/main/java/cse305/web/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.cse305.controller;
+package cse305.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/home")
+    public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+    
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
